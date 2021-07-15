4343edfc3b223efb2719840b64f905efaabf14d7
Denz <denzeldst11@gmail.com>
2021-07-15 14:34:18 -0400

    made apiKey random, added data- att, added event listener, added randomNum gen for games_count, made 2nd fetch, randomly selected single game, added init

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const gameSelectorApp = {};
 
 gameSelectorApp.url = "https://api.rawg.io/api/";
-gameSelectorApp.apiKey = "7feeb951031042d2a91e438eb2177a85";
+gameSelectorApp.apiKey = Math.random() < 0.5 ? "6e50074dadd14fa6b73a252ddd912030" : "7feeb951031042d2a91e438eb2177a85";
+// Returns random key, console.log(gameSelectorApp.apiKey);
 
 gameSelectorApp.fetchGameType = (type) => {
   const url = new URL(`${gameSelectorApp.url}${type}`);
@@ -32,6 +33,8 @@ gameSelectorApp.displayType = (data) => {
         newOptionEl.setAttribute("value", genre.id);
         selectEl.append(newOptionEl);
       });
+
+      gameSelectorApp.fetchRandomGame();
   }
   else{
       const selectEl = document.getElementById("platforms");
@@ -39,10 +42,73 @@ gameSelectorApp.displayType = (data) => {
         const newOptionEl = document.createElement("option");
         newOptionEl.textContent = platform.name;
         newOptionEl.setAttribute("value", platform.id);
+        newOptionEl.setAttribute("data-gamesCount", platform.games_count);
         selectEl.append(newOptionEl);
       });
   }
 };
 
-gameSelectorApp.fetchGameType("platforms");
-gameSelectorApp.fetchGameType("genres");
+
+gameSelectorApp.fetchRandomGame = () => {
+    const formEl = document.querySelector('form');
+
+    formEl.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        const platformChoiceVal = document.querySelector('[name="platforms"] option:checked').value;
+        console.log(platformChoiceVal)
+
+        // *Note 'gamescount' cannot be camelcase in data-*
+        const gamesCountVal = document.querySelector('[name="platforms"] option:checked').dataset.gamescount;
+        console.log(gamesCountVal)
+
+        const genreVal = document.querySelector('[name="genres"] option:checked').value;
+        console.log(genreVal)
+
+        //RANDOM NUMBER <10,000 Games - 250 * 40 = 10,000
+        const randomNum = (Math.floor(Math.random() * gamesCountVal/40)) + 6;
+
+        const randomNumMax250 = Math.min(randomNum, 250) - Math.floor(Math.random() * 6);
+
+        //FETCH
+        const url = new URL(`${gameSelectorApp.url}games`);
+        url.search = new URLSearchParams({
+            key: gameSelectorApp.apiKey,
+            platforms: `${platformChoiceVal}`,
+            page_size: 40, //40 is max api will allow
+            page: randomNumMax250
+        });
+
+        fetch(url)
+        .then( (response) => {
+            return response.json();
+        })
+        .then( (dataGames) => {
+            console.log("Random page of games", dataGames.results)
+            gameSelectorApp.displayGames(dataGames);
+
+        });
+    })
+
+}
+
+
+gameSelectorApp.displayGames = (dataGames) => {
+
+    const singleRandomGame = dataGames.results[Math.floor(Math.random() * dataGames.results.length)];
+
+    console.log("single random game", singleRandomGame);
+    
+
+
+}
+
+
+gameSelectorApp.init = () => {
+    gameSelectorApp.fetchGameType("platforms");
+    gameSelectorApp.fetchGameType("genres");
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    gameSelectorApp.init();
+})
